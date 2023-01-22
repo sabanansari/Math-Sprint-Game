ef4cb8fe2df15218c384bcce668849bbec60a466
sabanansari <alisaban52@gmail.com>
2023-01-22 18:56:17 +0530

    added fn to automatic scroll,start/stop timer,check score in times including penalty

--- script.js
+++ script.js
@@ -21,6 +21,7 @@ const playAgainBtn = document.querySelector('.play-again');
 // Equations
 let questionAmount;
 let equationsArray = [];
+let playerGuessArray = [];
 
 // Game Page
 let firstNumber = 0;
@@ -29,8 +30,57 @@ let equationObject = {};
 const wrongFormat = [];
 
 // Time
+let timer;
+let timePlayed = 0;
+let baseTime = 0;
+let penaltyTime = 0;
+let finalTime = 0;
+let finalTimeDisplay = '0.0s';
 
 // Scroll
+let valueY = 0;
+
+// Stop Timer, Process Results, go to Score Page
+function checkTime(){
+  if(playerGuessArray.length == questionAmount){
+    clearInterval(timer);
+    // Check for wrong guesses, add penalty time
+    equationsArray.forEach((equation,index)=>{
+      if(equation.evaluated === playerGuessArray[index]){
+        // Correct Guess, No Penalty
+      }else{
+        // Incorrct Guess, Add Penalty
+        penaltyTime += 0.5;
+      }
+    });
+    finalTime = timePlayed + penaltyTime;
+  }
+}
+
+// Add a tenth of a second to timePlayed
+function addTime(){
+  timePlayed += 0.1;
+  checkTime();
+}
+
+// Start timer when game is clicked
+function startTimer(){
+  // Reset times
+  timePlayed =0;
+  penaltyTime = 0;
+  finalTime = 0;
+  timer = setInterval(addTime, 100);
+  gamePage.removeEventListener('click',startTimer);
+}
+
+// Scroll and Store user selection in playerGuessArray
+function select(guessedTrue){
+  // Scroll 80 pixels
+  valueY+=80;
+  itemContainer.scroll(0,valueY);
+  // Add player guess to array
+  return guessedTrue ? playerGuessArray.push('true') : playerGuessArray('false');
+}
 
 // Displays Game Page
 function showGamePage(){
@@ -173,3 +223,5 @@ startForm.addEventListener('click', () =>{
 // Event Listeners
 
 startForm.addEventListener('submit',selectQuestionAmount);
+
+gamePage.addEventListener('click',startTimer);
